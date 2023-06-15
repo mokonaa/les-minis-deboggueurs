9842e2916fa21b379a37a023234055a07bbace24
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-15 14:18:32 +0200

    (feat:) mise en place du router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Partie from "./components/Partie/Partie";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/partie" element={<Partie/>} />
+            </Routes>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/components/Partie/Partie.jsx
@@ -0,0 +1,11 @@
+
+export default function Game() {
+
+  return (
+      <>
+          <div>
+              Game
+          </div>
+      </>
+  )
+}
