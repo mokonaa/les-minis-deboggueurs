39a2aeb2a78fa8af486996710d4d38adde3442b0
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-17 22:49:52 +0200

    (feat:) gestion des conditions des affichages des étapes de la configuration de la partie)

--- src/components/Configuration/Configuration.jsx
+++ src/components/Configuration/Configuration.jsx
@@ -1,14 +1,16 @@
 
 import { data } from '../../data/data';
+import { useState, useRef } from 'react';
 
 export default function Configuration() {
 
     let listJoueurs = data.enfants;
     let listAnimateurs = [];
     let thematique = data.thematique;
-    let nbJoueursChoisi = 0;
     let tabJoueursChoisis = [];
     let tabAnimateursChoisis = [];
+    let [etapesConfiguration, setEtapesConfiguration] = useState(0);
+    let [nbJoueursChoisi, setNbJoueursChoisi] = useState(0);
 
     // recup de la liste des animateurs 
    for (const property in thematique) {
@@ -17,14 +19,16 @@ export default function Configuration() {
             listAnimateurs.push(listAnimateurParThematique[index]);
         }
    }
-   console.log(listAnimateurs);
 
     const choisirNombreJoueur = (nbJoueur) => {
-        nbJoueursChoisi = nbJoueur; 
-        //console.log('nb joueur total : '+nbJoueursChoisi);
+        //nbJoueursChoisi = nbJoueur; 
+        setNbJoueursChoisi(nbJoueur);
+        console.log('nb joueur total : '+nbJoueursChoisi);
         tabJoueursChoisis = [];
         console.log(document.getElementsByTagName('button'));
         document.getElementById('joueurTotal_'+nbJoueur).style.backgroundColor = "#4fab78";
+        // changer le numéro de l'étape de la config 0 -> 1
+        setEtapesConfiguration(1);
     }
 
     const choixJoueur = (joueur) => {
@@ -54,26 +58,55 @@ export default function Configuration() {
         // console.log('joueurs choisis : '+tabJoueursChoisis.length);
     }
 
+    const changerEtapeConfiguration = (etape) => {
+      setEtapesConfiguration(etape);
+    }
+
     return (
         <>
             <div>
                 Configuration
-              <div>
-                <p>Combien de personnes jouent ? </p>
-                <button id='joueurTotal_2' onClick={() => choisirNombreJoueur(2)}>2</button>
-                <button id='joueurTotal_3' onClick={() => choisirNombreJoueur(3)}>3</button>
-                <button id='joueurTotal_4' onClick={() => choisirNombreJoueur(4)}>4</button>
-              </div>
+           
+              {/* CHOIX DU NOMBRE DE JOUEURS */}
+              { etapesConfiguration === 0 && (
+                    <div id='choixNombreJoueur'>
+                        <p>Combien de personnes jouent ? </p>
+                        <button id='joueurTotal_2' onClick={() => choisirNombreJoueur(2)}>2</button>
+                        <button id='joueurTotal_3' onClick={() => choisirNombreJoueur(3)}>3</button>
+                        <button id='joueurTotal_4' onClick={() => choisirNombreJoueur(4)}>4</button>
+                    </div>
+               )}
 
-              <div id='listJoueursEnfants'>
-                     <p>Quels joueurs jouent ?</p>
-                    {listJoueurs.map(enfant => <div className='enfantInfos' id= {'joueur_'+enfant.nom} onClick={() => choixJoueur({enfant})} >{enfant.nom}</div>)}
-              </div>
+              {/* CHOIX DES JOUEURS */}
+              { etapesConfiguration === 1 && (
+                    <div id='listJoueursEnfants'>
+                            <p>Quels joueurs jouent ?</p>
+                            <div>{listJoueurs.map(enfant => <div className='enfantInfos' id= {'joueur_'+enfant.nom} onClick={() => choixJoueur({enfant})} >{enfant.nom}</div>)}</div>                            
+                            <button  onClick={() => changerEtapeConfiguration(2)}>Continuer</button>
+                    </div>
+              )}
 
-              <div id='listAnimateurs'>
-                    <p>Quels animateurs jouent ?</p>
-                    {listAnimateurs.map(animateur => <div className='animateurInfos' id= {'animateur_'+animateur.nom} onClick={() => choixAnimateur({animateur})} >{animateur.nom}</div>)}
-              </div>
+              {/* CHOIX DES ANIMATEURS */}
+              { etapesConfiguration === 2 && (
+                    <div id='listAnimateurs'>
+                            <p>Quels animateurs jouent ?</p>
+                            {listAnimateurs.map(animateur => <div className='animateurInfos' id= {'animateur_'+animateur.nom} onClick={() => choixAnimateur({animateur})} >{animateur.nom}</div>)}
+                            <button onClick={() => changerEtapeConfiguration(3)}>Débuter la partie</button>
+                    </div>
+              )}
+
+
+              {/* INSTALL DU PLATEAU DE JEU  */}
+              { etapesConfiguration === 3 && (
+                    <div>
+                        <p>Installez le plateau</p>
+                        <ul>
+                            <li>1 - Déposez les cartes quizz dans l'emplacement prévue </li>
+                            <li>2 -  </li>
+                        </ul>
+                        <button>C'est fait !</button>
+                    </div>
+              )}
             </div>
         </>
     )
