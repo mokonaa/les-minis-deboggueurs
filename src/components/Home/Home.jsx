236e7e2056776f7d0c0bae65415e773d272e11ca
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-18 12:58:08 +0200

    (feat:) css configuration partie

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,18 @@
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-
     const navigate = useNavigate();
     const navigateTo = (route) => {
-        console.log('navigate to');
         navigate(route);
     }
 
     return (
-        <>
-            <div>
-                Home
+        <div id="">
+            <div className='accueilBtns'>
                 <button  onClick={() => navigateTo("/partie")} >Commencer une partie</button>
                 <button  onClick={() => navigateTo("/quizzs")} >Carte Quizz</button>
                 <button onClick={() => navigateTo("/galerie")} >Galerie</button>
             </div>
-        </>
+        </div>
     )
 }
