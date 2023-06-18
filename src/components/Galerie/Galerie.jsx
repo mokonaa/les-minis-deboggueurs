aa5b37134c75254f6a519a007cd633b8e14af2e4
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-18 19:23:54 +0200

    (feat:) affichage de la liste des thématiques dans la galerie

--- src/components/Galerie/Galerie.jsx
+++ src/components/Galerie/Galerie.jsx
@@ -1,10 +1,31 @@
 
+import { data } from '../../data/data';
+
 export default function Galerie() {
+
+    let listThematiques = data.thematique;
+    let tabThematiques = [];
+    console.log(listThematiques);
+
+   // recup de la liste des animateurs 
+   for (const property in listThematiques) {
+        let listThematique = listThematiques[property];
+        tabThematiques.push(listThematique);
+   }
+   console.log(tabThematiques);
+
+    const choixThematique = (thematique) => {
+    
+    }
+
 
     return (
         <>
             <div>
                 Galerie
+                <div id='listThematiques'>
+                    {tabThematiques.map(thematique => <div className='thematiquesInfos' id= {'thematique'+thematique.nomThematique} onClick={() => choixThematique({thematique})} >{thematique.nomThematique}</div>)}
+                </div>
             </div>
         </>
     )
