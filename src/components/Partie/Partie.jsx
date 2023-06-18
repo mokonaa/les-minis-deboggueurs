b8e6c89757fa1ba44d3482c73ae3b8e96d3cb5ea
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-18 16:57:41 +0200

    (feat:) mise en place du menu burger

--- src/components/Partie/Partie.jsx
+++ src/components/Partie/Partie.jsx
@@ -1,20 +1,7 @@
-import { data } from '../../data/data';
 import Configuration from '../Configuration/Configuration';
 import Animation from '../Animation/Animation';
 
 export default function Partie() {
-
-    console.log(data);
-    let joueurs = data.enfants;
-
-    const choisirJoueur = () => {
-        console.log('im here');
-        console.log(data);
-    }
-
-    console.log(joueurs);
-    joueurs.map(enfant => console.log(enfant));
-    //choisirJoueur();
 
     // TODO gestion des affichages (affichers d'abord component config puis animation)
   return (
