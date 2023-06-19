e77a66444040b507e377d1b45c4d18d28d4afb2d
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-19 09:53:46 +0200

    (feat:) css liste des thematiques dans galerie

--- src/components/Partie/Partie.jsx
+++ src/components/Partie/Partie.jsx
@@ -3,12 +3,12 @@ import Animation from '../Animation/Animation';
 
 export default function Partie() {
 
+    // TODO envoyer param enfantPreChoisiConfig (sous forme de tableaux d'objets)
     // TODO gestion des affichages (affichers d'abord component config puis animation)
   return (
           <div>
                 <Configuration/>
                 <Animation/>
-              
           </div>
   )
 }
