73fcebf777ced4673adbfe8727b8cbeaf0c26bc0
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-20 11:34:53 +0200

    (feat:) gestion des affichages des bonnes et mauvaises réponses

--- src/components/PanelAction/PanelAction.jsx
+++ src/components/PanelAction/PanelAction.jsx
@@ -33,11 +33,32 @@ export default function PanelAction({animateurs,thematiques}) {
         console.log(randomNumber);
         console.log(thematiques[nomThematique].questions[randomNumber]);
         setQuestionQuizz(thematiques[nomThematique].questions[randomNumber]);
+
+        let containersAnimateurs = document.getElementsByClassName('animateurInfos');
+        console.log(containersAnimateurs);
+        console.log(typeof containersAnimateurs);
+        //containersAnimateurs.map(animateurDiv => animateurDiv.style.background = 'red');
+        for (let i=0; i<containersAnimateurs.length; i++) {
+            console.log(containersAnimateurs[i]);
+            containersAnimateurs[i].style.background = "white";
+            containersAnimateurs[i].style.color = "black";
+
+            console.log(containersAnimateurs[i].id);
+            if (containersAnimateurs[i].id == "animateur_"+nomThematique) {
+                containersAnimateurs[i].style.background = "#433162";
+                containersAnimateurs[i].style.color = "white";
+            }
+        }
+
+
     }
 
     const ajoutReponse = (index) => {
         if (!tabReponseJoueurs.includes(index)) {
             tabReponseJoueurs.push(index);
+            let reponseContainer = document.getElementById('reponse'+index);
+            reponseContainer.style.background = '#e375a3';
+            reponseContainer.style.color = 'white';
         }
         else {
             console.log('already added');
@@ -76,6 +97,27 @@ export default function PanelAction({animateurs,thematiques}) {
 
         document.getElementById('validBtn').style.display = 'none';
         // TODO boucler et n'affichez que les réponses choisies par le joueur
+        let reponses = document.getElementsByClassName('reponses');
+
+        for (let i=0; i<reponses.length; i++) {
+            reponses[i].style.background = '#e06f4d';
+            reponses[i].style.color = 'white';
+        }
+
+        if (Array.isArray(questionQuizz.bonne_reponse)) {
+            for (let i=0; i<questionQuizz.bonne_reponse.length; i++) {
+                console.log(questionQuizz.bonne_reponse[i]);
+                document.getElementById('reponse'+questionQuizz.bonne_reponse[i]).style.background = '#4fab78';
+            }
+        }
+        else {
+            console.log(document.getElementById('reponse'+questionQuizz.bonne_reponse[0]));
+            console.log(questionQuizz.bonne_reponse);
+            document.getElementById('reponse'+questionQuizz.bonne_reponse).style.background = '#4fab78';
+            document.getElementById('reponse'+questionQuizz.bonne_reponse).style.color = 'white';
+        }
+    
+
         // TODO CSS
     }
 
@@ -96,7 +138,7 @@ export default function PanelAction({animateurs,thematiques}) {
                             <p className="remarque">À noter que la question quizz n’est disponible que lors de la récupération d‘un point </p>
 
                             <div className="listAnimateurChoisis">
-                                {animateurs.map(animateur => <div className="animateurInfos" onClick={() => displayQuestion(animateur.nomThematique)}>{animateur.nom}</div>)}
+                                {animateurs.map(animateur => <div id={"animateur_"+animateur.nomThematique} className="animateurInfos" onClick={() => displayQuestion(animateur.nomThematique)}>{animateur.nom}</div>)}
                             </div>
 
                             <button className="buttonConfirmer"  onClick={() => questionQuizz.choix !== undefined ? setDisplayQuestionView(true) : console.log('nothing to display')}>Super !</button>
@@ -105,36 +147,37 @@ export default function PanelAction({animateurs,thematiques}) {
 
                     {displayQuestionView && (
                         <div id="questionQuizz">
-                        <p className="subTitle">QUESTION QUIZZ</p>
-                        <p className="nomThematique">{animateursQuestionsInfos.nomThematique}</p>
-                        <div>
-                            <p className="questionTitre">{questionQuizz.titre}</p>
-                            {questionQuizz.choix.multiples ?
-                                 (<div className="listReponses">{questionQuizz.choix.multiples.map((choix,i) => <p id={"reponse"+i} onClick={() => ajoutReponse(i)}>{choix}</p>)}</div>)
-                                 :
-                                 (<div className="listReponses">{questionQuizz.choix.map((choix,i) => <p id={"reponse"+i} onClick={() => ajoutReponse(i)}>{choix}</p>)}</div>)
-
-                            }
-                            <button className="buttonConfirmer" id="validBtn" onClick={() => verifieReponse()}>Valider</button>
-                        </div>
-
-                        {/* BONNE REPONSE */}
-                        {bonneReponse === 1 && (
+                            <p className="subTitle">QUESTION QUIZZ</p>
+                            <p className="nomThematique">{animateursQuestionsInfos.nomThematique}</p>
+                            
                             <div>
-                                <p>Bravo ! Tu peux récupérer un point</p>
-                                <p>Pour plus d’informations n’hésite pas à aller dans la galerie pour trouver des ressources</p>
-                                {/*<!--<button className="buttonConfirmer" >Fermer</button>-->*/}
-                                </div>
-                        )}
+                                <p className="questionTitre">{questionQuizz.titre}</p>
+                                {questionQuizz.choix.multiples ?
+                                    (<div className="listReponses">{questionQuizz.choix.multiples.map((choix,i) => <p  className="reponses" id={"reponse"+i} onClick={() => ajoutReponse(i)}>{choix}</p>)}</div>)
+                                    :
+                                    (<div className="listReponses">{questionQuizz.choix.map((choix,i) => <p className="reponses"  id={"reponse"+i} onClick={() => ajoutReponse(i)}>{choix}</p>)}</div>)
 
-                        {/* MAUVAISE REPONSE */}
-                        {bonneReponse === 0 && (
-                            <div>
-                                <p>Mince, pas de point pour cette fois</p>
-                                <p>Pour plus d’informations n’hésite pas à aller dans la galerie pour trouver des ressources</p>
-                                {/*<!--<button className="buttonConfirmer" >Fermer</button>-->*/}
+                                }
+                                <button className="buttonConfirmer" id="validBtn" onClick={() => verifieReponse()}>Valider</button>
                             </div>
-                        )}
+
+                            {/* BONNE REPONSE */}
+                            {bonneReponse === 1 && (
+                                <div className="resultsContainer" id="resultsContainer_bon">
+                                    <p className="result">Bravo ! Tu peux récupérer un point</p>
+                                    <p className="infos">Pour plus d’informations n’hésite pas à aller dans la galerie pour trouver des ressources</p>
+                                    {/*<!--<button className="buttonConfirmer" >Fermer</button>-->*/}
+                                </div>
+                            )}
+
+                            {/* MAUVAISE REPONSE */}
+                            {bonneReponse === 0 && (
+                                <div className="resultsContainer" id="resultsContainer_mauvais">
+                                    <p className="result">Mince, pas de point pour cette fois</p>
+                                    <p className="infos">Pour plus d’informations n’hésite pas à aller dans la galerie pour trouver des ressources</p>
+                                    {/*<!--<button className="buttonConfirmer" >Fermer</button>-->*/}
+                                </div>
+                            )}
                         
                     </div>
                     )
