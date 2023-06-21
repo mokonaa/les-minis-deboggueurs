cfde5942ed1ab02a678c3aaebc40725f52139a48
“Aline <hy.aline@outlook.com>
2023-06-21 09:38:25 +0200

    (feat:) revue la gestion des points de vie

--- src/components/FicheJoueur/FicheJoueur.jsx
+++ src/components/FicheJoueur/FicheJoueur.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react';
 import React from 'react';
 import coeurPleinSrc from '../../assets/img/coeur_plein.svg';
 import coeurVideSrc from '../../assets/img/coeur_vide.svg';
+import Modal from '../Modal/Modal';
 
 
-export default function FicheJoueur({ nbDeplacements, pouvoir, nom, nbVie, description, maudit, objectif, role, img, nomThematique, objectifImg, joueurApres }) {
+export default function FicheJoueur({ nbDeplacements, pouvoir, nom, nbVie, description, maudit, objectif, role, img, nomThematique, objectifImg, enfantsTab }) {
+
+
+    const [showPopInEnfants, setShowPopInEnfants] = useState(false);
+
+    const displayPopIn = () => {
+        console.log('Display Pop In Animateurs');
+        setShowPopInEnfants(true);
+        console.log(showPopInEnfants);
+    }
 
     if (maudit === true) {
         maudit = "oui"
@@ -13,20 +23,24 @@ export default function FicheJoueur({ nbDeplacements, pouvoir, nom, nbVie, descr
     }
 
     // gestion du nombre de vie selon nbVie du joueurActuel avec une image now it works
-    
+
     const genererCoeurs = (nbVies, coeur_plein, coeur_vide) => {
         const coeursTab = [];
-        const nbViesMax = nbVies; // valeur que je souhaite stocker
+        const nbViesMax = nbVies + 1; // valeur que je souhaite stocker
         console.log("----- Points de vie Max -----");
         console.log(nbViesMax);
         console.log("----------");
+
+        // Là ca génère le nombre de vie
         for (let i = 0; i < nbVies; i++) {
-          coeursTab.push(coeur_plein);
+            coeursTab.push(coeur_plein);
         }
-        if(coeursTab.length < nbViesMax) {
-          coeursTab.push(coeur_vide);
+
+        if (coeursTab.length < nbViesMax) {
+            coeursTab.push(coeur_vide);
         }
-      
+
+
         return coeursTab;
     };
 
@@ -34,17 +48,58 @@ export default function FicheJoueur({ nbDeplacements, pouvoir, nom, nbVie, descr
     // Méthode pour supprimer un point de vie --> ca pas prio gérer si il en a plus console.log(joueurActuel est OUT faut l'enlever de OrdreFinal)
 
     // const [pointDeVie, setPointDeVie] = useState();
-    
-    const retirerPointDeVie = (joueurApres) => {
-        const pvStocker = joueurApres.pv;
-        const pvRetirer = pvStocker - 1;
-        console.log("----- Retirer Point de vie -----");
-        console.log(pvRetirer);
-        console.log("----------");
+
+    const [showPunition, setPunition] = useState(false);
+    const [message, setMessage] = useState("");
+
+    // il faut pendant l'affichage de la Modal, afficher le nombre de vies qu'il reste aux personnages
+    const [messagePoint, setMessagePV] = useState(false);
+
+    const retirerPointDeVie = (joueurChoisi) => {
+        // match le nom du joueurChoisi et le nom dans le tableau enfantsTab
+        const nomJoueurChoisi = joueurChoisi.nom;
+        const indexEnfantTrouve = enfantsTab.findIndex(
+            (enfantPunie) => enfantPunie.nom === nomJoueurChoisi
+        );
+        if (indexEnfantTrouve !== -1) {
+            joueurChoisi.pv--;
+            let message = `${nomJoueurChoisi} a ${joueurChoisi.pv} points de vie !`;
+            if (joueurChoisi.pv <= 0) {
+                message = `${nomJoueurChoisi} n'a plus de vie !`;
+                enfantsTab.splice(indexEnfantTrouve, 1);
+                setTimeout(() => {
+                    setPunition(true);
+                    setShowPopInEnfants(false);
+                }, 3500);
+            }
+            setMessage(message);
+        }
     }
-    
+
+    // const onClickConsole = (text, vara) => {
+    //     console.log(text);
+    //     console.log(vara);
+    // }
+
     return (
         <div id='ficheJoueur'>
+            {showPopInEnfants && (
+                <Modal setShowModal={setShowPopInEnfants}>
+                    {/* il faut que je réupere tous les enfantsChoisis */}
+                    {enfantsTab && (
+                        <>
+                            <p className="title">Sélectionner l'enfant que l'animateur punie</p>
+                            {console.log(enfantsTab)}
+                            {enfantsTab.map((enfant, i) =>
+                                <div key={i} id="ficheEnfantPunition" onClick={() => retirerPointDeVie(enfant)}>
+                                    <p>{enfant.nom}</p>
+                                    <p>{message}</p>
+                                </div>
+                            )}
+                        </>
+                    )}
+                </Modal>
+            )}
             {role === 'animateur' && (
                 <div id='ficheAnimateur'>
                     <div className='tour-wrapper'>
@@ -76,7 +131,7 @@ export default function FicheJoueur({ nbDeplacements, pouvoir, nom, nbVie, descr
                             </div>
                         </div>
                     </div>
-                    <button className="actions__button" onClick={retirerPointDeVie(joueurApres)}>Renvoyez un enfant dans le dortoir</button>
+                    <button className="actions__button" onClick={displayPopIn}>Renvoyez un enfant dans le dortoir</button>
                 </div>
             )}
             {role === 'enfant' && (
--- src/components/Suivi/Suivi.jsx
+++ src/components/Suivi/Suivi.jsx
@@ -2,79 +2,61 @@ import { useState, useEffect } from 'react';
 import { data } from '../../data/data';
 import FicheJoueur from '../FicheJoueur/FicheJoueur';
 import PanelAction from '../PanelAction/PanelAction';
-
-
-export default function Suivi({ joueursChoisis, animateursChoisis }) { // param -> data passé par Maria 
-
+export default function Suivi({ joueursChoisis, animateursChoisis }) { // param -> data passé par Maria
     /**
-     * 
+     *
      * params : tableau des animateurs trié , tableau des enfants choisies
-     * 
-     * Conditions d'affichage de vues 
+     *
+     * Conditions d'affichage de vues
      *  - Affichage des infos de l'animateur
      *  - Affichage statique Tours joueurs  - Jet de dés + gestion de l'ordre des joueurs
      *  - Switch entre l'affichage Joueur et affichage Animateur (prise en compte des priorités des animateurs et de l'ordre des enfants)
      *  - Affichage statique -> Manche terminée + Btn Commencer la nouvelle manche
     */
-
     // Récupération de la data via Configuration, pour le moment création d'une fake data en triant les animateurs selon leur ordre
-
     //let animateursTries = [];
     //const thematiques = data.thematique;
     let animateursTries = animateursChoisis;
     // Il faut récupérer chaque animateur de chaque thématique
-
     /*
     Object.keys(thematiques).forEach((theme) => {
         const animateurs = thematiques[theme].animateurs;
         animateursTries.push(...animateurs);
     });
     //*/
-
     // Tri des animateurs par priorité (ordre décroissant)
     animateursTries.sort((a, b) => b.priorite - a.priorite);
-
     // console.log('----- Animateurs Triés en ordre décroissant -----');
     // console.log(animateursTries);
     // console.log('-------------------------------------------------');
-
     // Sélection de l'ordre des joueurs selon le nb de joueurs, il faut récupérer cb de joueurs jouent
-
     //
     //const enfants = data.enfants;
     const enfants = joueursChoisis;
-
     // fausse data en attendant de recevoir
     const [enfantsChoisis, setEnfantsChoisis] = useState(enfants);
     // Le nb de joueurs choisis
     const nbDeJoueurs = enfants.length;
-
     const [roleActuel, setRoleActuel] = useState('animateur'); // par défaut animateur, // /!\ L'animateur qui a la plus grosse priorité commence toujours en premier
     // Gestion des manches et des tours (animateur -> enfant -> animateur -> enfant etc...)
     const [nbManches, setNbManches] = useState(1);
     const nbManchesMax = 10;
     const [nbTours, setNbTours] = useState(nbDeJoueurs * 2);
     const [nbTourActuel, setNbTourActuel] = useState(0);
-
     const deplacerJoueur = (index, nouvelIndex) => {
         const joueursEnfantCopies = [...enfantsChoisis];
         const joueurDeplace = joueursEnfantCopies.splice(index, 1)[0];
         joueursEnfantCopies.splice(nouvelIndex, 0, joueurDeplace);
         setEnfantsChoisis(joueursEnfantCopies);
     };
-
     const [ordreFinal, setOrdreFinal] = useState([]);
-
     // Il faut générer un nouvel ordre pour la suite du jeu
     const genererOrdreJoueurs = (ordreJoueursEnfant, animateursTries) => {
         const nouvelOrdre = [];
-
         const nbJoueurs = ordreJoueursEnfant.length;
         const nbAnimateurs = animateursTries.length;
-
         // Déterminer le nombre de tours nécessaire pour combiner tous les joueurs
         const nbTours = Math.max(nbJoueurs, nbAnimateurs);
-
         // Combinaison des joueurs "enfant" et des animateurs en alternance
         for (let i = 0; i < nbTours; i++) {
             const joueurEnfant = ordreJoueursEnfant[i % nbJoueurs]; // modulo % est utilisé pour revenir au début des tableaux lorsque l'index dépasse leur longueur
@@ -84,7 +66,6 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
         }
         return nouvelOrdre;
     };
-
     // nbTours -> correspond au nombre de tours dans une manche
     // nbTourActuel -> c'est le tour actuel de la partie si on est au tour 1 ou tour 2
     const gestionNbTours = () => {
@@ -92,14 +73,12 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
         setNbTourActuel(nbTourActuel + 1);
         if (nbTourActuel % 2 === 0) {
             setRoleActuel('animateur');
-
         } else {
             setRoleActuel('enfant');
         }
         // Vérifie si la manche est terminée après chaque tour
         gestionNbManche();
     };
-
     const gestionNbManche = () => {
         // Une manche se termine au bout du tours calculés selon le nb de Joueurs calculés plus tôt
         if (nbTourActuel >= nbTours) {
@@ -107,43 +86,33 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
             setNbTourActuel(1);
             setRoleActuel('animateur');
         }
-        if (nbManches > nbManchesMax) {
-            setNbTourActuel(0);
-            setNbManches(0);
+        if (nbManches > nbManchesMax && enfantsChoisis.length === 0) {
+            setNbManches(11); 
         }
     };
-
-
     const joueurActuel = nbTourActuel > 1 ? ordreFinal[nbTourActuel - 1] : animateursChoisis[0];
     // État pour stocker l'ordre des joueurs "enfant"
     const [ordreJoueursEnfant, setOrdreJoueursEnfant] = useState([]);
-
-
     const thematiques = data.thematique;
     const nomDuJoueurActuel = joueurActuel.nom ? joueurActuel.nom : animateursChoisis[0].nom;
     const [thematiqueActuel, setThematiqueActuel] = useState('');
-
     // Récupérer le nom de tous les animateurs et ensuite le comparer au nom du joueur actuel pour récup la bonne thématique
     const recupThematiqueActuel = () => {
         const entriesTab = Object.keys(thematiques).map((key) => thematiques[key]);
         const nomDesAnimateurs = [];
-
         for (let i = 0; i < entriesTab.length; i++) {
             const animateurs = entriesTab[i].animateurs;
             for (let j = 0; j < animateurs.length; j++) {
                 nomDesAnimateurs.push(animateurs[j].nom);
             }
         };
-
         const animateurTrouve = nomDesAnimateurs.find(
             (nomAnimateur) => nomAnimateur === nomDuJoueurActuel
         );
-
         if (animateurTrouve) {
             const thematiqueTrouvee = entriesTab.find((thematique) =>
                 thematique.animateurs.find((animateur) => animateur.nom === animateurTrouve)
             );
-
             if (thematiqueTrouvee) {
                 const thematique = thematiqueTrouvee.nomThematique;
                 if (thematique !== thematiqueActuel) {
@@ -153,16 +122,13 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
         }
     };
     console.log(thematiqueActuel);
-
     recupThematiqueActuel();
-
     // Fonction pour gérer la sélection de l'ordre des joueurs "enfant"
-    // Il faut que je teste et que j'affiche "les enfants" qui jouent 
+    // Il faut que je teste et que j'affiche "les enfants" qui jouent
     const selectionnerOrdreJoueursEnfant = (selectionOrdreJoueurs) => {
         setOrdreJoueursEnfant(selectionOrdreJoueurs);
     };
     const [afficherSelection, setSelection] = useState(true); // Variable d'état pour afficher/cacher la div d'ordre
-
     const validerOrdre = () => {
         selectionnerOrdreJoueursEnfant(enfantsChoisis);
         // A partir d'ici on récupère l'ordre que les enfants ont choisis
@@ -171,22 +137,17 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
         setSelection(false);
         setNbTourActuel(nbTourActuel + 1);
     };
-
-
-
     useEffect(() => {
         // console.log(joueursChoisis);
         // console.log(animateursChoisis);
     }, [joueursChoisis])
-
-
     // Il faut aussi penser à chaque déplacement, panel d'actions dont un seul qui est réellement intéractif (les questions)
     // Bouton Tour terminé selon cliquable dès qu'ils ont fait au moins toutes les actions qu'ils peuvent faire
     return (
         <>
             <div>
                 <h3>{nbManches}e Manche - {nbTourActuel}e Tour - role actuel : {roleActuel}</h3>
-                {nbTourActuel === 0 && afficherSelection &&
+                {nbTourActuel === 0 && afficherSelection && (
                     <div>
                         <h2>l'ordre des enfants</h2>
                         <ul>
@@ -200,13 +161,13 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
                         </ul>
                         <button onClick={validerOrdre}>Valider l'ordre</button>
                     </div>
-                } 
+                )}
                 {!afficherSelection &&  (
                     <div>
                         {roleActuel === 'enfant' ?
                             <>
                                 <FicheJoueur nbDeplacements={joueurActuel.deplacement} pouvoir={joueurActuel.pouvoir} nom={joueurActuel.nom} nbVie={joueurActuel.pv} description={joueurActuel.description} nomThematique={thematiqueActuel} maudit={""} objectif={""} role={roleActuel} img={joueurActuel.image} />
-                                <PanelAction animateurs={animateursTries} />
+                                <PanelAction animateurs={animateursTries} thematiques={data.thematique} enfantsTab={ordreJoueursEnfant}/>
                             </>
                             :
                             roleActuel === 'animateur' && (
@@ -222,8 +183,11 @@ export default function Suivi({ joueursChoisis, animateursChoisis }) { // param 
                         <button onClick={gestionNbTours}>Passer au prochain tour</button>
                     </div>
                 )}
+                {nbManches === 11 && (
+                    <p>C'est fini c'est perdu !</p>
+                )}
             </div>
-
         </>
     )
 }
+
