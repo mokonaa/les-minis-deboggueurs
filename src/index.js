f9408c0ddfa809c8188549a3f426ee34e2dc108d
mguydefontgalland <mariaguydefontgalland@gmail.com>
2023-06-21 15:46:59 +0200

    (feat:) changement du router de BrowserModule je ne sais plus quoi à hASHrOUTER

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import '../src/styles/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App/>
-    </BrowserRouter>
+    </HashRouter>
 );
